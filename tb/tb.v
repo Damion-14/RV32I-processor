8723db0b0e9d42e7a2db8ce063407751682d0e24
Damion Timothy HUPPERT <dhuppert@linux-2635.cae.wisc.edu>
2025-11-05 16:51:24 -0600

    NEW(.)(.)

--- tb/tb.v
+++ tb/tb.v
@@ -99,7 +99,7 @@ module hart_tb ();
 
         // Load the test program into memory at address 0.
         $display("Loading program.");
-        $readmemh("program.mem", imem);
+        $readmemh("/filespace/d/dhuppert/RV32I-processor/tb/program.mem", imem);
 
         // Reset the dut.
         $display("Resetting hart.");
